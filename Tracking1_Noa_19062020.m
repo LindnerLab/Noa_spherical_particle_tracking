94495c94d0562c3c282deabf53aa6b5f36670dc6
Noa-Burshtein <66832003+Noa-Burshtein@users.noreply.github.com>
2020-06-19 17:00:55 +0200

    Add files via upload

---
+++ Tracking1_Noa_19062020.m
@@ -0,0 +1,124 @@
+% This code uses an convolutional method to determine the locations of
+% circular particles in an image, given an expected particle radius, r.
+% Particles can be separated from false positives by comparing any of the
+% 'regionprops' properties to a reference value and selecting only the
+% particles that have a property that is 'greater' or 'smaller' than the
+% reference 'Value'. Examples of properties are 'Area' and 'Eccentricity'.
+% More info can be found on https://www.mathworks.com/help/images/ref/regionprops.html
+% The codes find circles for each frame of an image sequence
+% Presenting the circles that are found in the center plane!
+% Saving the coordinates of each circle for each image in a .txt file
+
+clc;
+close all;
+clear all;
+
+% Read the whole image sequence
+image_folder = 'C:\Users\labo\Desktop\Noa\water-glycerol_80um_190620\5ml_min_3000fps_24C\raw_images';    %  Enter name of folder from which you want to upload pictures 
+verbose = true;                                         % If verbose is true, the script will create and save a figure of the particle finding
+r = 5;                                                 % Expected radius of the particles in pixels
+threshold = 0.2;                                        % Threshold for the binerization of the normalized convoluted image.
+
+SelectionCriteria = struct('Property',[],'Value',[],'Criteria',[]);
+% The particle shouldn't be too small
+SelectionCriteria(1).Property = 'Area';
+SelectionCriteria(1).Value = pi*(0.8*r)^2;
+SelectionCriteria(1).Criteria = 'Greater';
+
+% The particle shouldn't be too large either
+SelectionCriteria(2).Property = 'Area';
+SelectionCriteria(2).Value = pi*(1.2*r)^2;
+SelectionCriteria(2).Criteria = 'Smaller';
+
+% The particle should be roughly round (0 is perfectly round, 1 is a line)
+SelectionCriteria(3).Property = 'Eccentricity';
+SelectionCriteria(3).Value = 0.4;
+SelectionCriteria(3).Criteria = 'Smaller';
+
+
+%% Below This No Input Are Required!
+folder = pwd;
+filenames = [dir(fullfile(image_folder, '*.tif'))];     % read all images with specified extention
+total_images = numel(filenames);                        % count total number of photos present in that folder
+
+mkdir raw_images_crop
+mkdir particles_marked
+mkdir particles_coordinates
+
+% crop images
+full_name = fullfile(image_folder, filenames(5).name);  % specify images names with full path and extension
+Img_temp = imread(full_name);                           % Read img need to indicate crop
+[Img_temp, rect] = imcrop(Img_temp);                    % Manually crop the example image and save the crop
+Img_size = size(Img_temp);                              % Determine the size of the cropped image (needed for pre-allocating memory).
+
+Img = zeros(Img_size(1)+60,Img_size(2)+60, total_images);     % Pre-allocate memory for the cropped images (is faster than allocating space when needed.
+clear Img_temp Img_size                                 % Delete the temporary image, as it is no longer needed
+
+for n = 1:total_images
+    full_name = fullfile(image_folder, filenames(n).name);      % specify images names with full path and extension
+    Img_temp = double(imcrop(imread(full_name),rect));
+    Img(:,:,n) = padarray(Img_temp,[30 30],0,'both');        % Import, crop, convert the image to double and invert it
+end
+
+% calculate average image for background removal      
+% Subtract background and invert image
+
+Img_inv = 2^16 - Img + 1;
+Img_inv(Img_inv < 0) = 0;
+average_fig = mean(Img_inv,3);                          % Take the average over all the images (averages values of all pixels with the same z-index)
+Img_bg = Img_inv - average_fig;                         % Subtract the background signal
+%Img_bg = histeq(Img_bg);
+
+% pass images through a filter, find circles and save x,y coordinates to *.txt files
+for n = 1:total_images    
+    % Find circles
+    Img_bpass = bpass(Img_bg(:,:,n),1,40);            % filter - smoothing and subtracting the background (picture, 1, need to adjust to fit with particle diameter)
+    Img_bpass = Img_bpass - 20;
+    Img_bpass(Img_bpass < 0) = 0;
+    
+  %  [allcenters1] = FindParticlesConvolution(Img_bpass,r,SelectionCriteria, threshold, false);
+    [centers1, radii1, metric1] = imfindcircles(Img_bpass,[5,15]); %finds the particles with defined centers - the particles in the focus plane. [ radii range ]
+    allcenters1 = centers1(:,:);
+    allm1 = metric1(:);
+  %  viscircles(allcenters1,allm1, 'EdgeColor','r');  
+  
+  
+    baseFileName = sprintf('%03d',n);                   % Create string with image number padded with zeros   
+    if verbose && mod(n,10) == 0
+        % Plot original image
+        figure(n);
+        subplot(2,1,1)
+        colormap('gray'),imagesc(Img(:,:,n))
+        title('Original Image')
+        daspect([1 1 1])
+        set(gcf, 'Visible', 'off') %dont show figure while code is running
+        set(gcf, 'color', 'w');
+
+        % Plot inverted and filtered image (with background removed)
+        subplot(2,1,2)
+        colormap('gray'); imagesc(uint16(Img_bpass)); % display image
+        set(gcf, 'Visible', 'off') %dont show figure while code is running
+        title('Detected particles')
+
+        % Indicate the found particles
+        viscircles(allcenters1,ones(size(allcenters1,1),1)*r, 'EdgeColor','r','LineWidth',1);    
+    %             %%If there are 2 particle sizes in the video             
+    %             [centers2, radii2, metric2] = imfindcircles(b,[40,60]); %finds the particles with defined centers - the particles in the focus plane. [ radii range ]
+    %             allcenters2 = centers2(:,:);
+    %             allm2 = metric2(:);
+    %             viscircles(allcenters2,allm2, 'EdgeColor','r');
+        daspect([1 1 1])
+
+        % Save the combined figure
+        fullFileName = fullfile(folder,'particles_marked', [baseFileName,'.tif']);
+        saveas(gcf, fullFileName,'tiff');
+    end
+    
+    % Save cropped images
+    fullFileName = fullfile(folder,'raw_images_crop',  [baseFileName,'.tif']);
+    imwrite(uint16(Img(:,:,n)), fullFileName);
+    
+    % write the coordinates of the particles in each image to a seperate file
+    fullFileName = fullfile(folder,'particles_coordinates', [baseFileName,'.txt']);
+    dlmwrite(fullFileName,allcenters1,'delimiter','\t','precision',3);
+end
---
+++ Tracking2_Noa_19062020.m
@@ -0,0 +1,194 @@
+% http://htmlpreview.github.io/?https://raw.githubusercontent.com/tinevez/simpletracker/master/publish/html/TestSimpleTracker.html
+% This code plots the data from all .txt files found by the code
+% Tracking_Noa_18042020 - giving an indication of all the particles that
+% were detected finding their trajectories and plotting them
+%
+%The code finds the velocity of each particle along the trajectory and
+% plots it
+
+clc;close all;clear all;
+
+pix2micro = 3.48101E-06;    %distance in pixel=316.0396, distance=1.10E-03m - pixel/m 
+%average_flow_velocity = 7;        %ml/minute
+U = 6.89E-02; %m/sec % !!!!!!! Adjust according to experiment
+fps = 3000; % !!!!!!! Adjust according to experiment
+dt = 1/fps;
+w = 1.10E-03; %channel width 
+nx = 1024;
+ny = 512;
+
+% Define x,y coordinates and normalize
+x = 0.6;
+y = 0.6;
+
+
+v_mag = zeros(nx,ny);
+v_all = zeros(nx,ny);
+
+
+% Read the whole image sequence
+file_folder = 'C:\Users\labo\Desktop\Noa\water-glycerol_80um_190620\5ml_min_3000fps_24C\particles_coordinates';    %  Enter name of folder from which you want to upload pictures with full path
+folder = pwd;
+filenames = dir(fullfile(file_folder, '*.txt'));                               % read all images with specified extention
+total_files = numel(filenames);                                                % count total number of photos present in that folder
+%mkdir particles_marked
+
+points = cell(total_files,1); % allocating a cell array for saving all the data collected in the txt files
+
+% %figure (1) % Plots all detected particles in black 
+% % for n = 1:total_files
+% % 
+% %     full_name= fullfile(file_folder, filenames(n).name);           % specify images names with full path and extension
+% %     fid = fopen(full_name, 'r');                   % Read images
+% %     Mydata = textscan(fid,'%f %f');           % Skip header lines
+% %     fclose(fid);
+% %     Mydata=cell2mat(Mydata); % this can be plotted to see all the particles detected in an overlay
+% %     points(n,:) = {Mydata}; % contains all coordinates of all points found in a single file to perform tracking from here.   
+% %     %plot((Mydata(:,1)*pix2micro/w)-1.6,(Mydata(:,2)*pix2micro/w)-0.8,'o', 'MarkerSize', 3, 'LineWidth', 1); hold on 
+% %     daspect([1 1 1])
+% %     set(gcf, 'color', 'w');
+% %     xlim([-1.2 1.2])
+% %     ylim([-0.5 0.5])
+% %     xlabel('\it y / w ');  ylabel('\it z / w'); 
+% % end
+% % title('All detected particles')
+
+
+for n = 1:total_files % Plots all detected particles in black (background for trajectories)
+
+    full_name= fullfile(file_folder, filenames(n).name);           % specify images names with full path and extension
+    fid = fopen(full_name, 'r');                   % Read images
+    Mydata = textscan(fid,'%f %f');           % Skip header lines
+    fclose(fid);
+    Mydata=cell2mat(Mydata); % this can be plotted to see all the particles detected in an overlay
+    points(n,:) = {Mydata}; %contains all coordinates of all points found in a single file to perform tracking from here.   
+    plot((Mydata(:,1)*pix2micro/w)-x,((Mydata(:,2)*pix2micro/w)-y),'o', 'MarkerSize', 2, 'LineWidth', 1, 'color', 'k'); hold on % Hold the plot normalize axis and align vortex center to x,y (0,0)
+    daspect([1 1 1])
+    set(gcf, 'color', 'w');
+    
+end
+
+
+%linking between the coordinates with Nearestneighbor method.
+max_linking_distance = 100; %Inf; defines the maximal possible length of a trajectory
+max_gap_closing = 1; % defines maximal linking gap
+debug = true;
+
+[ tracks, adjacency_tracks ] = simpletracker(points,...
+    'MaxLinkingDistance', max_linking_distance, ...
+    'MaxGapClosing', max_gap_closing, ...
+    'Debug', debug);
+
+adjacency_tracks_filter = adjacency_tracks(cellfun('length',adjacency_tracks)>=5); % determine the minimum length of a trajectory (to plot)
+tracks_filter = adjacency_tracks(cellfun('length',adjacency_tracks)>=5); % determine the minimum length of a trajectory (to plot)
+
+% Plot tracks - plot each track in a given color. Normally we would have to retrieve the points coordinates in the given |points| initiall cell
+% arrat, for each point in frame. To skip this, we simple use the adjacency_tracks, that can pick points directly in the concatenated
+% points array |all_points|.
+
+n_tracks = numel(adjacency_tracks_filter);
+colors = jet(n_tracks);
+all_points = vertcat(points{:});
+all_points = round(all_points);
+all_track_points = zeros(length(all_points),2);
+
+figure (1)
+for i_track = 2 : 1 : n_tracks   % Use the adjacency tracks to retrieve the points coordinates.
+       
+    track = adjacency_tracks_filter{i_track};
+    track_points = all_points(track, :);
+    all_track_points(track,:) = all_points(track, :); 
+    
+    plot((track_points(:,1)*pix2micro/w)-x, (track_points(:, 2)*pix2micro/w)-y, '-', 'Color', colors(i_track, :),'MarkerSize', 2, 'LineWidth', 1); hold on; %normalize axis and align vortex center to x,y (0,0)
+    daspect([1 1 1])
+    set(gcf, 'color', 'w');
+    xlim([-0.5 0.5])
+    ylim([-0.5 0.5])
+    xlabel('\it y / w ');  ylabel('\it z / w'); 
+    title('Calculated trajectories')
+
+end
+
+
+  figure (2)  %plot the trajectories with the velocity of each segment
+  numColors = 255; % resolution of color map
+  cmap = jet(numColors);
+  colormap(jet(numColors));
+  
+  for i_track = 1 : 1 : n_tracks% find velocity of each particle (linked to a trajectory) as a function of time
+
+        track = adjacency_tracks_filter{i_track}; % find the filtered tracks
+        track_points = all_points(track, :); % find all points from each track
+        % v_average = zeros(size(i_track,adjacency_tracks_filter(1)));
+        
+        
+        distance = zeros(size(track_points(1))); % allocate a space to save the distance a particle traveled between frames
+        distanceX = zeros(size(track_points(1))); % allocate a space to save the distance X a particle traveled between frames
+        distanceY = zeros(size(track_points(1))); % allocate a space to save the distance Y a particle traveled between frames
+        v = zeros(size(track_points(1))); % allocate a space to find the velocity of a particle
+        
+        for k = 1 : length(track_points)-1    % length of each trajectory
+            %             t(k) = (k-1)*dt; % find the total time a particle was detected
+            distanceX(k) = ((abs  (track_points ((k+1),1) )) - (abs (track_points (k,1)) ))^2;  %calculate the distance of a particle between frames (k-2) is used for smoothing
+            distanceY(k) = ((abs  (track_points ((k+1),2) )) - (abs (track_points (k,2)) ))^2;  %calculate the distance of a particle between frames (k-2) is used for smoothing
+            distance = sqrt(distanceX + distanceY);
+            v = (distance*pix2micro) / dt;
+            track_points((k+1),3) = v(k);
+            v_mag(track_points((k+1),1),track_points((k+1),2)) =   track_points((k+1),3);
+            v_all(k,i_track) = v(k); % collect all velocities in a matrix
+        end
+   
+         vMax = max(v_all(:))/U; % Find the maximal velocity of all the trajectories
+         %vMax = 1;
+         
+         for k = 1 : length(track_points)-2
+             vAvg = ((track_points((k+1),3) + track_points((k+2),3))/2)/U;  % calculate average velocity for this segment
+             if   vAvg ~= 0 % this is to avoid data that is 0
+                 if vAvg < 2 %vMax/3 %to avoid extremely high velocity values determined by a single vMax value
+                     idx = ceil((vAvg/(2)*numColors)); % look up color for current velocity normalize by maximum velocity  %    idx = ceil(vAvg/vMax*numColors); % normalize by maximum velocity
+                     color = cmap(idx,:);
+                     plot([(track_points(k+1,1)*pix2micro/w)-x,(track_points((k+2),1)*pix2micro/w)-x],[(track_points(k+1,2)*pix2micro/w)-y,(track_points((k+2),2)*pix2micro/w)-y],'o-','MarkerSize', 4, 'Color',color, 'LineWidth', 2);   hold on;
+                 end
+             end
+         end
+  end
+ 
+  
+            h = colorbar;
+            ylabel(h, '\it v_{mag} / U', 'FontName', 'Times')
+            caxis([0  vMax/2]) 
+            daspect([1 1 1])
+            set(gcf, 'color', 'w');
+            set(gca,'FontSize', 16, 'FontName', 'Times', 'LineWidth',1.5)
+            xlim([-0.5 0.5])
+            ylim([-0.5 0.5])
+            set(gca,'YTick',-0.5:0.5:0.5);
+            xlabel('\it y / w ');  ylabel('\it z / w');
+            %   legend ();
+  %          title('Trajectories with velocity of each segment')
+
+            
+ % Collect points only from the tracks that were filtered 
+ all_track_points_x  = nonzeros(all_track_points(:,1));%(all_track_points ~= 0);
+ all_track_points_y  = nonzeros(all_track_points(:,2));%(all_track_points ~= 0);
+
+ 
+%figure(3)
+% plot the density of the particles
+%[bins in x, bins in y] 1024 x 512 pixel (25 pixel per bin - approx size of a particle)
+   %     densityplot((all_points(1:2:end,1)*pix2micro/w)-1.7,(all_points(1:2:end,2)*pix2micro/w)-0.8, [51,25]); %https://www.mathworks.com/matlabcentral/fileexchange/65166-densityplot-x-y-varargin 
+   densityplot((all_track_points_x(1:1:end)*pix2micro/w)-x,(all_track_points_y(1:1:end)*pix2micro/w)-y, [25,20]); %https://www.mathworks.com/matlabcentral/fileexchange/65166-densityplot-x-y-varargin
+   daspect([1 1 1])
+   h = colorbar;
+  % caxis([0 5])
+   ylabel(h, 'Number of particles', 'FontName', 'Times')
+   set(gcf, 'color', 'w');
+   set(gca,'Color',[0.0 0.0 0.6])
+   set(gca,'FontSize', 16, 'FontName', 'Times', 'LineWidth',1.5)
+   xlim([-0.4 0.35])
+   ylim([-0.35 0.37])
+   set(gca,'YTick',-0.35:0.35:0.35);
+   set(gca,'XTick',-0.35:0.35:0.35);
+   xlabel('\it y / w ');  ylabel('\it z / w');
+%   title('Particle density')
+
